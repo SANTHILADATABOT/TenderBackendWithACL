bc4bfc4d23a4f4f8d8ef8c2cf3991801b2428eb0
madhusudhanan <madhuraj@gmail.com>
2023-02-06 10:12:51 +0530

    06-02-2023

---
+++ app/Http/Controllers/FinancialEvaluationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FinancialEvaluation;
+use Illuminate\Http\Request;
+
+class FinancialEvaluationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FinancialEvaluation  $financialEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FinancialEvaluation $financialEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FinancialEvaluation  $financialEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FinancialEvaluation $financialEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FinancialEvaluation  $financialEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FinancialEvaluation $financialEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FinancialEvaluation  $financialEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FinancialEvaluation $financialEvaluation)
+    {
+        //
+    }
+}
