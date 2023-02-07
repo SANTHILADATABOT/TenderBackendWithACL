d865d679a20c05bda00579b793f481ddf73cbde6
Prabakaran V <prabakaranpbk@gmail.com>
2023-02-07 18:12:50 +0530

    Bidders  UI Completed

--- app/Http/Controllers/CompetitorProfileCreationController.php
+++ app/Http/Controllers/CompetitorProfileCreationController.php
@@ -180,7 +180,7 @@ if ($validator->fails()) {
         if($comps){
         $compList = [];
         foreach($comps as $comp){
-            $compList[] = ["value" => $comp['id'], "label" =>  $comp['compName'], "isdisabled" => false] ;
+            $compList[] = ["value" => $comp['id'], "label" =>  $comp['compName']] ;
         }
         return  response()->json([
             'status' => 200,
