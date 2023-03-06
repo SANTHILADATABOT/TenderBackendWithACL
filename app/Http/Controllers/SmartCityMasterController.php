d60463e7d2d18b933dfe6d99f2b358677a2cf308
Prabakaran V <prabakaranpbk@gmail.com>
2023-03-06 12:11:58 +0530

    Test

---
+++ app/Http/Controllers/SmartCityMasterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SmartCityMaster;
+use Illuminate\Http\Request;
+
+class SmartCityMasterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SmartCityMaster  $smartCityMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SmartCityMaster $smartCityMaster)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SmartCityMaster  $smartCityMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SmartCityMaster $smartCityMaster)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SmartCityMaster  $smartCityMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SmartCityMaster $smartCityMaster)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SmartCityMaster  $smartCityMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SmartCityMaster $smartCityMaster)
+    {
+        //
+    }
+}
---
+++ app/Models/SmartCityMaster.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SmartCityMaster extends Model
+{
+    use HasFactory;
+}
